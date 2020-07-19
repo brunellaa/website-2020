b76d54363a606372497035ddd6a77638063d7c12
Alejandro Brunella <alejandro.brunella@gmail.com>
2020-07-19 20:58:01 -0300

    Updates

---
+++ precache-manifest.1c3316f19fad3e4666a744a6a5b49d9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e686e5178110d66550a91ee382e252",
+    "url": "/react-developer-website/index.html"
+  },
+  {
+    "revision": "63cd7997eb09fd63f067",
+    "url": "/react-developer-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0fd5ad94ed16d616a48f",
+    "url": "/react-developer-website/static/js/2.8685b082.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-developer-website/static/js/2.8685b082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63cd7997eb09fd63f067",
+    "url": "/react-developer-website/static/js/main.819b6d09.chunk.js"
+  },
+  {
+    "revision": "41b189359604a004789d",
+    "url": "/react-developer-website/static/js/runtime-main.ca5a0468.js"
+  }
+]);
